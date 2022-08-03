7a09adefadb995deea2aec9a74414033534dc759
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-03 11:01:52 -0300

    ejercicio1resuelto

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,10 +1,12 @@
 from asyncore import read
 from re import T
+from time import time
 from turtle import distance
 
 
 print ("Ingrese la velocidad y el tiempo de la unidad movil")
 
-read = v = 100
-read T=60 min
-distance =(v * T)
+V = float( input("Velocidad: "))
+T= int(input("tiempo: "))
+D= V * T
+print (D)
