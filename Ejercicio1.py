ab132ce180180653f51e71536c6199752862614a
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-03 11:04:20 -0300

    EJERCICIOVELOCIDAD

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,11 +1,4 @@
-from asyncore import read
-from re import T
-from time import time
-from turtle import distance
-
-
 print ("Ingrese la velocidad y el tiempo de la unidad movil")
-
 V = float( input("Velocidad: "))
 T= int(input("tiempo: "))
 D= V * T
