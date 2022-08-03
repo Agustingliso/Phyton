59135fa735d64416abaf1c9fb7e644810a567f22
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-03 10:45:00 -0300

    ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+from asyncore import read
+from re import T
+from turtle import distance
+
+
+print ("Ingrese la velocidad y el tiempo de la unidad movil")
+
+read = v = 100
+read T=60 min
+distance =(v * T)
