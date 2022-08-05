f8b7c4225907c7f3cc67ce2d79a5778a57259de5
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-05 09:49:22 -0300

    ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+print ("Partidos ganados: ")
+pg= int (input())
+print ("Partidos empatados: ")
+pe= int (input())
+print ("Partidos perdidos: ")
+pp= int (input())
+
+pg= pg*3
+pe= pe*1
+pp= pp*0
+pt= pg+pe+pp
+print ("El puntaje total es de:", pt)
