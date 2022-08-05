51cfc691b8492a2617a809b7ca6e1df10092564d
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-05 10:09:32 -0300

    ejercicio5

---
+++ ejercicio5.py
@@ -0,0 +1,14 @@
+from asyncore import read
+
+
+print ("Ingrese la cantidad de GB: ")
+GB= float (input())
+mg=GB*1.024
+md=mg/1.44
+
+print ("nSALIDA: ")
+print ("-----------------------")
+print (md)
+
+print ("la cantidad de discos necesarios es: " )
+
