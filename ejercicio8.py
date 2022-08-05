85d0f057d957dd766f4f0e165ebab5b6449870d6
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-05 10:47:52 -0300

    ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+base = float (input ("Ingresa la base del rectangulo: "))
+altura= float (input ("Ingresa la altura del rectangulo: "))
+sup= base*altura
+print ("La superficie es de: ", sup )
+per= 2* (base + altura)
+print ("Superficie: ", sup)
+print ("Perimetro: ", per)
