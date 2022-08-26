a20d269c45bd15ca5eaa4effac2da1e4a8956d44
Agustingliso <108811464+Agustingliso@users.noreply.github.com>
2022-08-26 11:03:20 -0300

    elementos

---
+++ sumamatrices.py
@@ -0,0 +1,24 @@
+
+print("Suma de matrices")
+A = []
+B = []
+C = []
+
+print ("Dimension, maximo 100")
+S = int( input("Filas: "))
+R = int( input("Columnas: "))
+
+for i in range(S):
+ A.append( [] ) 
+ B.append( [] ) 
+ C.append( [] ) 
+for j in range(R):
+ A[i].append( int( input("A{}{}: ".format(i+1,j+1))))
+ B[i].append( int( input("B{}{}: ".format(i+1,j+1))))
+ C[i].append( A[i][j] + B[i][j])
+
+print("\nSALIDA: ")
+
+print(A)
+print(B)
+print(C)
